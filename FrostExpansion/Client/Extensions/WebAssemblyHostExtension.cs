aa0a43c2982c3ffbfefb17f80ff4fb46855f794b
jahmashy <maksym.mroz99@gmail.com>
2023-05-11 22:10:02 +0200

    InitialCommit

---
+++ FrostExpansion/Client/Extensions/WebAssemblyHostExtension.cs
@@ -0,0 +1,29 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
+
+namespace Frost.Client.Extensions
+{
+    public static class WebAssemblyHostExtension
+    {
+        public async static Task SetDefaultCulture(this WebAssemblyHost host)
+        {
+            var localStorage = host.Services.GetRequiredService<ILocalStorageService>();
+            var cultureFromLS = await localStorage.GetItemAsync<string>("culture");
+
+            CultureInfo culture;
+
+            if (cultureFromLS != null)
+            {
+                culture = new CultureInfo(cultureFromLS);
+            }
+            else
+            {
+                culture = new CultureInfo("pl-PL");
+            }
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+    }
+}
